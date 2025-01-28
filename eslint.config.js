d0815882dd050392bad33ae16a64b1f62be766ea
Anton Ovsiannikov <ovsiannikov@managed-code.com>
2025-01-29 01:32:37 +0200

    Fix type error in absolute path

--- eslint.config.js
+++ eslint.config.js
@@ -7,30 +7,30 @@ import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [
-      js.configs.recommended, 
-      ...tseslint.configs.recommended,
-      prettierConfig
-    ],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'prettier': prettierPlugin,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'prettier/prettier': 'error',
-    },
-  },
-)
+	{ ignores: ['dist'] },
+	{
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommended,
+			prettierConfig,
+		],
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+		},
+		plugins: {
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			prettier: prettierPlugin,
+		},
+		rules: {
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'prettier/prettier': 'error',
+		},
+	}
+);
