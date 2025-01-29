298b6227a7e6a0712632fec5dabf1a4cf865b01d
Anton Ovsiannikov <ovsiannikov@managed-code.com>
2025-01-29 16:36:15 +0200

    Init login\chat pages and rename home => welcome page. Install react-hook-form and yup

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
-import HomePage from '@pages/home';
+import WelcomePage from '@pages/welcome';
+import ChatPage from '@pages/chat';
 
 export default function Router() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route path="/" element={<WelcomePage />} />
+				<Route path="/chat/*" element={<ChatPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
